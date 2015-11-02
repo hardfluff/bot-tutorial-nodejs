4e55859ee5e167641bfece42453061e9333b0cc7
hardfluff <junkmailliam123@gmail.com>
2015-11-02 13:49:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,21 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
+  var request = JSON.parse('pls');,
+      botRegex = /^\.pls$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\.dank$/;
 
