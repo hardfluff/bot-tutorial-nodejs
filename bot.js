ae4c2f4b8f3ed4b41a1d9aa8368626c018d11356
hardfluff <junkmailliam123@gmail.com>
2015-11-02 15:32:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,26 +5,18 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\.dank$/;  botRegexPLS = /^\.pls$/;
+      botRegex = /^\.dank$/;  botPLS = /^\.pls$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-  if(request.text && botRegexPLS.test(request.text)) {
+  } 
+  if(request.text && botPLS.test(request.text)) {
     this.res.writeHead(200);
     postMessage("pls");
     this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+  } 
 }
 
 function postMessage() {
