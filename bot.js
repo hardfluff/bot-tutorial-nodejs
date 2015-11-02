8ce0f21fbd08b930f16dc6492c79fccb01b8ada8
hardfluff <junkmailliam123@gmail.com>
2015-11-02 15:06:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,10 @@ function respond() {
     this.res.writeHead(200);
     postMessage("pls");
     this.res.end();
+  }else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
   }
 }
 
